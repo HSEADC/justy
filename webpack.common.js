be47c4a35e4a0bd0fd3b1fb3e42da16f623142b0
KoUD2 <kostik-udod@mail.ru>
2023-10-28 16:21:16 +0300

    Index page

--- webpack.common.js
+++ webpack.common.js
@@ -19,6 +19,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
         test: /\.(js|jsx)$/i,
         exclude: /node_modules/,
         use: {
@@ -54,7 +65,7 @@ module.exports = {
         type: 'asset/source'
       },
       {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
         type: 'asset/resource',
         generator: {
           filename: 'images/[hash][ext][query]'
@@ -84,13 +95,96 @@ module.exports = {
       chunks: ['index']
     }),
 
-    // Internal pages
+    //Inspiration
     new HtmlWebpackPlugin({
-      hash: true,
-      scriptLoading: 'blocking',
-      template: './src/pages/page.html',
-      filename: './pages/page.html',
-      chunks: ['page']
+      template: './src/inspiration.html',
+      filename: './inspiration.html'
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/inspiration/studios.html',
+      filename: './inspiration/studios.html'
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/inspiration/media.html',
+      filename: './inspiration/media.html'
+    }),
+
+    //About
+    new HtmlWebpackPlugin({
+      template: './src/about.html',
+      filename: './about.html'
+    }),
+
+    //Technologies
+    new HtmlWebpackPlugin({
+      template: './src/technologies.html',
+      filename: './technologies.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/technologies/mockups.html',
+      filename: './technologies/mockups.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/technologies/neural-networks.html',
+      filename: './technologies/neural-networks.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/technologies/programs.html',
+      filename: './technologies/programs.html'
+    }),
+
+    //News
+    new HtmlWebpackPlugin({
+      template: './src/news.html',
+      filename: './news.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/news/channels.html',
+      filename: './news/channels.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/news/books.html',
+      filename: './news/books.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/news/films.html',
+      filename: './news/films.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/news/code.html',
+      filename: './news/code.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/news/recommendations.html',
+      filename: './news/recommendations.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/news/typography.html',
+      filename: './news/typography.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/news/our-articles.html',
+      filename: './news/our-articles.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/news/hse-help.html',
+      filename: './news/hse-help.html'
+    }),
+
+    //Contacts
+    new HtmlWebpackPlugin({
+      template: './src/contacts.html',
+      filename: './contacts.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/contacts/students.html',
+      filename: './contacts/students.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/contacts/job.html',
+      filename: './contacts/job.html'
     }),
 
     // Partials
