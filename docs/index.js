cb976fc9b4918e21dc495247e3cda7b3568dc364
KoUD2 <kostik-udod@mail.ru>
2023-12-17 21:32:06 +0300

    build new project

--- docs/index.js
+++ docs/index.js
@@ -7,15 +7,46 @@ var blade_1 = document.getElementsByClassName('first-fold');
 var blade_2 = document.getElementsByClassName('second-fold');
 var blade_3 = document.getElementsByClassName('third-fold');
 var blade_4 = document.getElementsByClassName('fourth-fold');
-var header = document.getElementsByClassName('header-article');
-var whalf = window.innerWidth / 2;
+var header = document.getElementsByClassName('verticle');
+var slides = Array.from(header[0].querySelectorAll('img'));
+var slideCount = slides.length;
+var whalf = 627 / 2;
+var slideIndex = 0;
 header[0].addEventListener('mousemove', function (e) {
-  if (e.pageX < whalf) {
-    header[0].style.cursor = 'pointer';
+  if (e.pageX - 109 < whalf) {
+    header[0].removeEventListener('click', showNextSlide);
+    header[0].classList.remove('right');
+    header[0].classList.add('left');
+    header[0].addEventListener('click', showPreviousSlide);
   } else {
-    header[0].style.cursor = 'alias';
+    header[0].removeEventListener('click', showPreviousSlide);
+    header[0].classList.remove('left');
+    header[0].classList.add('right');
+    header[0].addEventListener('click', showNextSlide);
   }
 });
+
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+  slides.forEach(function (slide, index) {
+    if (index === slideIndex) {
+      slide.style.opacity = '1';
+    } else {
+      slide.style.opacity = '0';
+    }
+  });
+}
+
+updateSlider();
 
 var _loop = function _loop(i) {
   folder[i].addEventListener('mouseenter', function () {
