a087c9e125bfa4da57639d0c3fdfa85cd0a1345b
KoUD2 <kostik-udod@mail.ru>
2023-12-27 18:20:59 +0300

    add mobile version to about page

--- docs/moveDown.js
+++ docs/moveDown.js
@@ -1,18 +1,22 @@
 /******/ (() => { // webpackBootstrap
 var __webpack_exports__ = {};
-var heightOfViewport = window.innerHeight;
-var fortyPercentOfViewport = heightOfViewport * 0.6;
-window.addEventListener('scroll', function () {
-  var firstCard = document.getElementById('card3');
-  var secondCard = document.getElementById('card4');
-  var watchedElement = document.getElementsByClassName('cards');
-  var targetHeight = fortyPercentOfViewport;
-  var rect = watchedElement[0].getBoundingClientRect();
+var viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
-  if (rect.top <= targetHeight) {
-    firstCard.classList.add('movedown');
-    secondCard.classList.add('movedown');
-  }
-});
+if (viewport_width > 912) {
+  var heightOfViewport = window.innerHeight;
+  var fortyPercentOfViewport = heightOfViewport * 0.6;
+  window.addEventListener('scroll', function () {
+    var firstCard = document.getElementById('card3');
+    var secondCard = document.getElementById('card4');
+    var watchedElement = document.getElementsByClassName('cards');
+    var targetHeight = fortyPercentOfViewport;
+    var rect = watchedElement[0].getBoundingClientRect();
+
+    if (rect.top <= targetHeight) {
+      firstCard.classList.add('movedown');
+      secondCard.classList.add('movedown');
+    }
+  });
+}
 /******/ })()
 ;
--- src/javascript/move-down.js
+++ src/javascript/move-down.js
@@ -1,16 +1,23 @@
-const heightOfViewport = window.innerHeight
-const fortyPercentOfViewport = heightOfViewport * 0.6
+const viewport_width = Math.max(
+  document.documentElement.clientWidth,
+  window.innerWidth || 0
+)
 
-window.addEventListener('scroll', () => {
-  const firstCard = document.getElementById('card3')
-  const secondCard = document.getElementById('card4')
-  const watchedElement = document.getElementsByClassName('cards')
-  const targetHeight = fortyPercentOfViewport
+if (viewport_width > 912) {
+  const heightOfViewport = window.innerHeight
+  const fortyPercentOfViewport = heightOfViewport * 0.6
 
-  const rect = watchedElement[0].getBoundingClientRect()
+  window.addEventListener('scroll', () => {
+    const firstCard = document.getElementById('card3')
+    const secondCard = document.getElementById('card4')
+    const watchedElement = document.getElementsByClassName('cards')
+    const targetHeight = fortyPercentOfViewport
 
-  if (rect.top <= targetHeight) {
-    firstCard.classList.add('movedown')
-    secondCard.classList.add('movedown')
-  }
-})
+    const rect = watchedElement[0].getBoundingClientRect()
+
+    if (rect.top <= targetHeight) {
+      firstCard.classList.add('movedown')
+      secondCard.classList.add('movedown')
+    }
+  })
+}
