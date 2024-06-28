cce7b2c6e20bba25a3ce67ad9eda1af5ac0449f1
Anastasiia <FT_Love@list.ru>
2024-06-28 16:25:43 +0400

    comit to docs old

---
+++ docs_old/page.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+// import './page.css'
+// import React from 'react'
+// import { createRoot } from 'react-dom/client'
+// import ComponentExample from './javascript/ComponentExample.jsx'
+// document.addEventListener('DOMContentLoaded', () => {
+//   const S_container = document.getElementById('reactComponentRoot')
+//   const root = createRoot(S_container)
+//   root.render(<ComponentExample />)
+// })
+/******/ })()
+;
